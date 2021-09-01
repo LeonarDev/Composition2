4e928bdfedde3e7814f557680ddd7ff6b5c0ec95
LeonarDev <leonardo.majevski@gmail.com>
2021-09-01 12:21:40 -0300

    init

---
+++ Composition2/Program.cs
@@ -0,0 +1,52 @@
+﻿using Composition2.Entities;
+using Composition2.Entities.Enums;
+using System;
+
+namespace Composition2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter client data:");
+            Console.Write("Name: ");
+            string clientName = Console.ReadLine();
+            Console.Write("Email: ");
+            string clientEmail = Console.ReadLine();
+            Console.Write("Birth date (DD/MM/YYYY): ");
+            DateTime clientBirthDate = DateTime.Parse(Console.ReadLine());
+            Client client = new Client(clientName, clientEmail, clientBirthDate);
+
+            Console.WriteLine("Enter order data:");
+            Console.Write("Status: ");
+            OrderStatus status = Enum.Parse<OrderStatus>(Console.ReadLine());
+
+            Order order = new Order(DateTime.Now, status, client);
+
+            Console.Write("How many times to this order? ");
+            int n = int.Parse(Console.ReadLine());
+
+            for (int i = 1; i <= n; i++)
+            {
+                Console.WriteLine($"Enter #{i} item data:");
+                Console.Write("Product name: ");
+                string productName = Console.ReadLine();
+                Console.Write("Product price: ");
+                double productPrice = double.Parse(Console.ReadLine());
+
+                Product product = new Product(productName, productPrice);
+
+                Console.Write("Quantity: ");
+                int productQuantity = int.Parse(Console.ReadLine());
+
+                OrderItem orderItem = new OrderItem(productQuantity, productPrice, product);
+
+                order.AddItem(orderItem);
+            }
+
+            Console.WriteLine();
+            Console.WriteLine("ORDER SUMMARY:");
+            Console.WriteLine(order);
+        }
+    }
+}
